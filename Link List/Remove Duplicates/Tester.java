5cb14ae75cdd1401f503742d6b0dd92ef3313c2d
Mansi <43803851+mansimm@users.noreply.github.com>
2021-01-13 15:51:57 +0530

    remove duplicates added

--- Link List/Remove Duplicates/Tester.java
+++ Link List/Remove Duplicates/Tester.java
@@ -1,1 +1,195 @@
+package linkelisttester1;
+
+
+class Node {
+
+    private String data;
+    private Node next;
+
+    public Node(String data) {
+        this.data = data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+      
+    public void setNext(Node node) {
+        this.next = node;
+    }
+      
+    public String getData() {
+        return this.data;
+    }
+      
+    public Node getNext() {
+        return this.next;
+    }
+}
+
+
+class LinkedList {
+
+    private Node head;
+    private Node tail;
+
+    public Node getHead() {
+        return this.head;
+    }
+      
+    public Node getTail() {
+        return this.tail;
+    }
+
+    public void addAtEnd(String data) {
+        Node node = new Node(data);
+
+        if (this.head == null) {
+            this.head = this.tail = node;
+        } else {
+            this.tail.setNext(node);
+
+            this.tail = node;
+        }
+    }
+
+    public void addAtBeginning(String data) {
+        Node node = new Node(data);
+
+        if (this.head == null) {
+            this.head = this.tail = node;
+        }
+
+        else {
+            node.setNext(this.head);
+            this.head = node;
+        }
+    }
+
+      public void display() {
+        Node temp = this.head;
+
+        while (temp != null) {
+            System.out.println(temp.getData());
+            temp = temp.getNext();
+        }
+    }
+
+    public Node find(String data) {
+        Node temp = this.head;
+
+        while (temp != null) {
+            if (temp.getData().equals(data))
+                return temp;
+            temp = temp.getNext();
+        }
+        return null;
+    }
+
+    public void insert(String data, String dataBefore) {
+        Node node = new Node(data);
+
+        if (this.head == null)
+            this.head = this.tail = node;
+        else {
+            Node nodeBefore = this.find(dataBefore);
+            if (nodeBefore != null) {
+                node.setNext(nodeBefore.getNext());
+                nodeBefore.setNext(node);
+                if (nodeBefore == this.tail)
+                    this.tail = node;
+            } 
+            else
+                System.out.println("Node not found");
+        }
+    }
+
+    public void delete(String data) {
+
+        if (this.head == null)
+            System.out.println("List is empty");
+        else {
+            Node node = this.find(data);
+
+            if (node == null)
+                System.out.println("Node not found");
+
+            if (node == this.head) {
+                this.head = this.head.getNext();
+                node.setNext(null);
+          
+                if (node == this.tail)
+                    tail = null;
+            }
+            else {
+                Node nodeBefore = null;
+                Node temp = this.head;
+                while (temp != null) {
+                    if (temp.getNext() == node) {
+                    nodeBefore = temp;
+                    break;
+                    }
+                    temp = temp.getNext();
+                }
+
+                nodeBefore.setNext(node.getNext());
+                      
+                if (node == this.tail)
+                    this.tail = nodeBefore;
+                node.setNext(null);
+            }
+        }
+    }
+}
+
+
+class Tester {
+
+    public static void main(String args[]) {
+        LinkedList child = new LinkedList();
+       
+        child.addAtEnd("Rahul");
+        child.addAtEnd("Seema");
+        child.addAtEnd("Geetu");
+        child.addAtEnd("Geetu");
+        child.addAtEnd("Y");
+        child.addAtEnd("Y");
+        child.addAtEnd("Y");
+        child.addAtEnd("tarun ");
+        child.addAtEnd("tom");
+        child.addAtEnd("tom");
+
+        System.out.println("Before");
+        child.display();
+        
+        Node head = child.getHead();
+        LinkedList child2 = new LinkedList();
+
+        String x="";
+        while(head!=null)
+        {
+        	if(head.getData().equals(x))
+        	{
+        		
+        	}
+        	else {
+        		child2.addAtEnd(head.getData());
+        		
+        	}
+        	x = head.getData();
+        	head=head.getNext();
+        	
+        }
+        System.out.println("	Linkelist without duplicates \n");
+        child2.display();
+        
+        
+       
+     
+    }
+
+   
+
+    
+}
 
